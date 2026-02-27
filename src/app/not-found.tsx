20ac19e9cbbef2c140cb7bbd294aab9d1257d0b1
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-02-27 11:23:39 +0200

    .

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,10 @@
-import { DogIcon } from 'lucide-react'
+import { BugIcon } from 'lucide-react'
 
 export default function NotFound() {
     return (
         <div className="wrapper flex min-h-full w-full shrink items-center justify-center">
             <div className="flex flex-col gap-y-8 text-center">
-                <DogIcon size={128} className="m-auto" />
+                <BugIcon size={128} className="m-auto" />
                 <h1 className="text-2xl font-bold">Страница не найдена</h1>
                 <p>
                     Возможно, она была перемещена,
